7fa8ac4570a9dd5e26b131bcad7d0976832cf4ac
Jrivass <144190039+Jrivass@users.noreply.github.com>
2023-09-15 19:28:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,115 @@
-// Vector de palabras aleatorias de 5 letras
-const palabras = ["TIGER", "LEMON", "MANGO", "GRAPE", "APPLE", "SNAKE", "BREAD", "PAPER"];
+let intentos = 6;
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH'];
+let palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
 
-// Palabra aleatoria para adivinar
-let palabra = palabras[Math.floor(Math.random() * palabras.length)];
+window.addEventListener('load', init);
 
-let intentosRestantes = 6;
-let letrasUsadas = [];
+function init() {
+  const button = document.getElementById("guess-button");
+  button.addEventListener("click", intentar);
+  const resetButton = document.getElementById("reset-button");
+  resetButton.addEventListener("click", reiniciarJuego);
 
-document.addEventListener('DOMContentLoaded', function() {
-    mostrarPalabra();
-});
-
-function mostrarPalabra(){
-    const wordDisplay = document.getElementById("word-display");
-    wordDisplay.textContent = palabra;
-}
-
-function intentar(){
-    const intento = leerIntento();
-    letrasUsadas.push(intento);
-    const resultado = compararPalabra(intento);
-    mostrarResultado(resultado);
-    actualizarIntentosRestantes();
-    
-    if (intentosRestantes === 0) {
-        terminarJuego("PERDISTE");
+  // Activar los botones al presionar "Enter"
+  const input = document.getElementById("guess-input");
+  input.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+      if (input.disabled) {
+        reiniciarJuego();
+      } else {
+        intentar();
+      }
     }
+  });
 }
 
-function leerIntento(){
-    const intento = document.getElementById("guess-input").value.toUpperCase();
-    return intento;
+function leerIntento() {
+  let intento = document.getElementById("guess-input");
+  intento = intento.value.toUpperCase();
+  return intento;
 }
 
-function compararPalabra(intento){
-    if (intento === palabra) {
-        return "GANASTE";
-    } else {
-        let resultado = "";
-        for (let i = 0; i < palabra.length; i++) {
-            if (intento[i] === palabra[i]) {
-                resultado += "VERDE";
-            } else if (palabra.includes(intento[i])) {
-                resultado += "AMARILLO";
-            } else {
-                resultado += "GRIS";
-            }
-        }
-        return resultado;
+function terminar(mensaje, ganaste) {
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    const BOTON = document.getElementById("guess-button");
+    BOTON.disabled = true;
+  
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+  
+    const reiniciarBoton = document.createElement('button');
+    reiniciarBoton.innerHTML = 'Reiniciar';
+    reiniciarBoton.id = 'reiniciar-button';
+    reiniciarBoton.addEventListener('click', function() {
+      location.reload(); // Recarga la página al hacer clic en el botón de reinicio
+    });
+    contenedor.appendChild(reiniciarBoton);
+  
+    // Escuchar el evento keydown en el documento
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'Enter' && event.target === reiniciarBoton) {
+        location.reload(); // Recarga la página cuando se presiona Enter en el botón de reinicio
+      }
+    });
+  }
+  
+  
+
+function reiniciarJuego() {
+    intentos = 6;
+    palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+  
+    const contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = '';
+  
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = false;
+    INPUT.value = ''; // Esto limpia el campo de entrada
+  
+    const BOTON = document.getElementById("guess-button");
+    BOTON.disabled = false;
+  
+    const reiniciarBoton = document.getElementById('reset-button');
+    reiniciarBoton.style.display = 'none';
+  }
+  
+  function intentar() {
+    const INTENTO = leerIntento();
+    if (INTENTO === palabra) {
+      terminar("<h1>¡GANASTE! 😀</h1>");
+      return;
     }
-}
-
-function mostrarResultado(resultado){
-    const grid = document.getElementById("word-display");
-    const letras = grid.getElementsByClassName("letter");
-    for (let i = 0; i < resultado.length; i++) {
-        letras[i].textContent = resultado[i];
-        letras[i].classList.add(resultado[i].toLowerCase());
+  
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+  
+    for (let i in palabra) {
+      const SPAN = document.createElement('span');
+      SPAN.className = 'letter';
+      if (INTENTO[i] === palabra[i]) {
+        SPAN.innerHTML = INTENTO[i];
+        SPAN.style.backgroundColor = '#79b851'; // Verde
+      } else if (palabra.includes(INTENTO[i])) {
+        SPAN.innerHTML = INTENTO[i];
+        SPAN.style.backgroundColor = '#f3c237'; // Amarillo
+      } else {
+        SPAN.innerHTML = INTENTO[i];
+        SPAN.style.backgroundColor = '#a4aec4'; // Gris
+      }
+      ROW.appendChild(SPAN);
     }
-}
-
-function actualizarIntentosRestantes(){
-    intentosRestantes--;
-    const guesses = document.getElementById("guesses");
-    guesses.textContent = `Attempts left: ${intentosRestantes}`;
-}
-
-function terminarJuego(mensaje){
-    const input = document.getElementById("guess-input");
-    const button = document.getElementById("guess-button");
-    input.disabled = true;
-    button.disabled = true;
-    const guesses = document.getElementById("guesses");
-    guesses.textContent = mensaje;
-}
-
+    GRID.appendChild(ROW);
+  
+    intentos--;
+  
+    // Limpia el campo de entrada
+    const INPUT = document.getElementById("guess-input");
+    INPUT.value = '';
+  
+    if (intentos === 0) {
+      terminar("<h1>¡PERDISTE! 😖</h1>");
+    }
+  }
+  
